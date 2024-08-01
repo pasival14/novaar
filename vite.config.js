cdb0cf03c75fa57cb48b134fd941f7ad20ef1394
Femi <pasival14@gmail.com>
2024-08-01 14:10:08 +0100

    4

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [svgr(), react()],
-  base : './novaar-landing-page/',
+  base : './novaar',
   css: {
    postcss: {
     plugins: [tailwindcss()],
